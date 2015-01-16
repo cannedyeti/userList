a18b4267ef4d3a01cec7bdb40318e00c60177f7b
Connor Potebnya <Merlin13>
2015-01-15 23:09:31 -0800

    Commit
    Having a hell of a time getting this working on Heroku. Keeps giving me
    errors. Can run a simple app, but not my own.

---
+++ user.js
@@ -0,0 +1,45 @@
+var usersList = angular.module('usersList', []);
+
+usersList.controller('userCtrl', function($scope){
+
+	function resetUser() {
+		$scope.user = {
+			firstName: "",
+			lastName: "",
+			email: ""
+		}
+	}
+
+	//adds user based on input
+	function addUser() {
+		$scope.userList.push($scope.user);
+		resetUser();
+	}
+
+	//delete user
+	function deleteUser(user) {
+		var index = $scope.userList.indexOf(user);
+		$scope.userList.splice(index, 1);
+	}
+
+
+	//edit current user
+	function editUser() {
+		$scope.editMode = true;
+	}
+
+	//update
+	function updateUser(){
+		$scope.editMode = false;
+	}
+
+	//current users
+	$scope.userList = [];
+
+
+
+	$scope.addUser = addUser;
+	$scope.deleteUser = deleteUser;
+	$scope.editUser = editUser;
+	$scope.updateUser = updateUser;
+});
